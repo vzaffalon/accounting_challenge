61743e49c8584ec839349b0053335b0799cf7eff
Victor Zaffalon <victor.zaffalon@udlab.com.br>
2020-05-09 19:14:03 -0300

    created login token logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,54 @@
 class ApplicationController < ActionController::Base
-    def authentication
-        return true
-    end
+    private def api_key_from_header
+        bearer_pattern = /^Bearer /
+        basic_pattern = /^Basic /
+        auth_header = request.env['HTTP_AUTHORIZATION']
+    
+        return unless auth_header
+    
+        if auth_header.match(bearer_pattern)
+          return auth_header.gsub(bearer_pattern, '')
+        else
+          token = Base64.decode64(auth_header.gsub(basic_pattern, ''))
+          return token.slice(0..(token.index(':'))).gsub(':', '') if token.include?(':')
+        end
+      end
+    
+      private def authenticate
+        if request.env['HTTP_AUTHORIZATION']
+          auth = Base64.decode64(request.env['HTTP_AUTHORIZATION'].gsub(/^Basic /, ''))
+        end
+    
+        @current_user_token = UserToken.find_by_token(api_key_from_header)
+    
+        if @current_user_token
+          if @current_user_token.expiry_at && @current_user_token.expiry_at < Time.current
+            @current_user_token.destroy
+            render json: { message: 'This token has expired.', code: 'invalid_token' }, status: :unauthorized
+            return false
+          else
+            @current_user_token.update_attributes(
+              expiry_at: @current_user_token.expiry_at ? Time.current + UserToken::TTL : nil,
+            )
+    
+            @current_user = @current_user_token.user
+            User.current = @current_user
+            unless @current_user
+              render json: { message: 'Invalid access token.', code: 'invalid_token' }, status: :unauthorized
+              return false
+            end
+          end
+        else
+          render json: { message: 'Invalid access token', code: 'invalid_token' }, status: :unauthorized
+          return false
+        end
+      end
+    
+      private def current_user
+        @current_user if defined?(@current_user)
+      end
+    
+      private def current_user_token
+        @current_user_token if defined?(@current_user_token)
+      end
 end
--- app/controllers/user_tokens_controller.rb
+++ app/controllers/user_tokens_controller.rb
@@ -1,3 +1,40 @@
 class UserTokensController < ApplicationController
-    before_action :authenticate
+    before_action :authenticate, only: :destroy
+
+    def create
+      if params[:email]
+        users = User.where(email: params[:email])
+        unless users.any?
+          return render json: { message: 'invalid_login', code: 'invalid_login' }, status: :unprocessable_entity
+        end
+
+        user = users.first
+
+        if user&.authenticate?(params[:password])
+          user_token = UserToken.create(
+            user_id: user.id,
+            expiry_at: Time.current + UserToken::TTL,
+            user_agent: request.user_agent,
+            create_ip: request.remote_ip
+          )
+
+          render json: { token: user_token.token }
+        else
+          render json: { message: 'invalid_login', code: 'invalid_login' }, status: :unprocessable_entity
+        end
+      else
+        render json: { message: 'invalid_login', code: 'invalid_login' }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      current_user_token.destroy
+      @current_user.user_tokens.destroy_all if params[:all]
+    end
+
+    private
+
+    def user_token_params
+      params.permit(:email, :password)
+    end
 end
