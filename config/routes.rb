3a63ee1f8c80478946397d9a9fe5a289e4ec955d
Victor Zaffalon <victor.zaffalon@udlab.com.br>
2020-05-10 15:45:43 -0300

    created routes for api requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'accounts', controller: 'accounts', action: 'index'
+  get 'account_transfers', controller: 'account_transfers', action: 'index'
+  get 'account_transactions', controller: 'account_transactions', action: 'index'
+
+
+  post 'accounts', controller: 'accounts', action: 'create'
+  post 'account_transfers', controller: 'account_transfers', action: 'create'
+  post 'account_transactions', controller: 'account_transactions', action: 'create'
+  post 'user_tokens', controller: 'user_tokens', action: 'create'
+  post 'user', controller: 'users', action: 'create'
 end
